8559b2bf74922491323460075740baafccbffe6e
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-27 11:29:27 -0700

    fix footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,31 +5,31 @@ import "../assets/css/footer.css";
 export default function Footer() {
     return (
         <footer>
-            <ul className="is-flex is-flex-wrap-nowrap is-justify-content-space-evenly is-align-content-center">
+            <ul className="is-flex is-flex-wrap-nowrap is-justify-content-space-between mx-4 is-align-items-baseline">
                 <li>
                     <a href="mailto:ryanmoscoe@gmail.com">
-                        <span className="contact-icon">
-                            <i className="fa-solid fa-envelope"></i>
+                        <span className="contact-icon mr-2 is-size-6-touch">
+                            <i className="fa-solid fa-envelope is-size-6-touch"></i>
                         </span>
-                        <span>ryanmoscoe@gmail.com</span>
+                        <span className="is-size-6-touch">ryanmoscoe@gmail.com</span>
                     </a>
                 </li>
                 <li>
                     <a href="https://www.linkedin.com/in/ryan-moscoe-8652973/" target="_blank" rel="noreferrer">
-                        <i className="fa-brands fa-linkedin"></i>
-                        <span>LinkedIn</span>
+                        <i className="fa-brands fa-linkedin is-size-6-touch"></i>
+                        <span className="ml-2 is-size-6-touch">LinkedIn</span>
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/rmoscoe" target="_blank" rel="noreferrer">
-                        <i className="fa-brands fa-github"></i>
-                        <span>GitHub</span>
+                        <i className="fa-brands fa-github is-size-6-touch"></i>
+                        <span className="ml-2 is-size-6-touch">GitHub</span>
                     </a>
                 </li>
                 <li>
                     <a href="https://medium.com/@ryanmoscoe" target="_blank" rel="noreferrer">
-                        <i className="fa-brands fa-medium"></i>
-                        <span>Medium</span>
+                        <i className="fa-brands fa-medium is-size-6-touch"></i>
+                        <span className="ml-2 is-size-6-touch">Medium</span>
                     </a>
                 </li>
             </ul>
--- src/components/nav.js
+++ src/components/nav.js
@@ -4,35 +4,38 @@ import "../assets/css/nav.css";
 export default function Nav({ currentPage, handlePageChange }) {
 
     return (
-        <nav>
-            <a
-                href="#about"
-                onClick={() => handlePageChange("About")}
-                className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-            >
-                About Me
-            </a>
-            <a
-                href="#portfolio"
-                onClick={() => handlePageChange("Portfolio")}
-                className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
-            >
-                Portfolio
-            </a>
-            <a
-                href="#contact"
-                onClick={() => handlePageChange("Contact")}
-                className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
-            >
-                Contact
-            </a>
-            <a
-                href="#resume"
-                onClick={() => handlePageChange("Resume")}
-                className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
-            >
-                Resume
-            </a>
-        </nav>
+        <header>
+            <nav>
+                <a
+                    href="#about"
+                    onClick={() => handlePageChange("About")}
+                    className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+                >
+                    About Me
+                </a>
+                <a
+                    href="#portfolio"
+                    onClick={() => handlePageChange("Portfolio")}
+                    className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
+                >
+                    Portfolio
+                </a>
+                <a
+                    href="#contact"
+                    onClick={() => handlePageChange("Contact")}
+                    className={currentPage === 'Contact' ? 'nav-link active' : 'nav-link'}
+                >
+                    Contact
+                </a>
+                <a
+                    href="#resume"
+                    onClick={() => handlePageChange("Resume")}
+                    className={currentPage === 'Resume' ? 'nav-link active' : 'nav-link'}
+                >
+                    Resume
+                </a>
+            </nav>
+        </header>
+
     )
 }
