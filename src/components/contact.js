2140d6cbe90981baa139605e5d97357d65e58d51
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-29 08:43:56 -0700

    add sendEmail function

--- src/components/contact.js
+++ src/components/contact.js
@@ -6,6 +6,45 @@ export default function Contact() {
     const [contactName, setcontactName] = useState('');
     const [message, setMessage] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [submitted, setSubmitted] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState();
+
+    const sendEmail = (data) => {
+        setLoading(true);
+        setSubmitted(false);
+        setErrorMessage(undefined);
+
+        fetch(endpointUrl, {
+            method: 'POST',
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data),
+        })
+            .then((response) => {
+                if (response.status === 422) {
+                    throw new Error("Are you a robot?");
+                }
+
+                if (response.status !== 200) {
+                    throw new Error(`${response.statusText} (${response.status})`);
+                }
+
+                return response.json();
+            })
+            .then(() => {
+                setSubmitted(true);
+                setLoading(false);
+            })
+            .catch((err) => {
+                setError(err.toString());
+                setLoading(false);
+            });
+
+        return { submitted, loading, error, sendEmail }
+    };
 
     const handleInputChange = (e) => {
         const { target } = e;
