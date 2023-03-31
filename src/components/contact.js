6d531938fa85d5cefa2510ed52460374fbd44e62
Ryan Moscoe <104706818+rmoscoe@users.noreply.github.com>
2023-03-31 14:36:03 -0700

    improve error and confirmation messaging on contact form

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,46 +10,49 @@ export default function Contact() {
     const [submitted, setSubmitted] = useState(false);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState();
+    const [confirmation, setConfirmation] = useState(false);
 
-    const sendEmail = (data) => {
+    const sendEmail = async (data) => {
         setLoading(true);
         setSubmitted(false);
         setNotification(undefined);
-
-        fetch("https://public.herotofu.com/v1/0c8d6c10-ce4a-11ed-9434-5b53665d9a66", {
-            method: 'POST',
-            headers: {
-                Accept: "application/json",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(data),
-        })
-            .then((response) => {
-                if (response.status === 422) {
-                    throw new Error("Are you a robot?");
-                }
-
-                if (response.status !== 200) {
-                    throw new Error(`${response.statusText} (${response.status})`);
-                }
-
-                setSubmitted(true);
-                setLoading(false);
-                
-                return response.json();
-            })
-            .catch((err) => {
-                setError(err.toString());
-                setLoading(false);
+        try {
+            const response = await fetch("https://public.herotofu.com/v1/0c8d6c10-ce4a-11ed-9434-5b53665d9a66", {
+                method: 'POST',
+                headers: {
+                    Accept: "application/json",
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(data),
             });
+            if (response.status === 422) {
+                throw new Error("Are you a robot?");
+            }
 
-        return { submitted, loading, error, sendEmail }
+            if (response.status !== 200) {
+                throw new Error(`${response.statusText} (${response.status})`);
+            }
+
+            setSubmitted(true);
+            setLoading(false);
+
+            return response.json();
+
+        } catch (err) {
+            setError(err.toString());
+            setLoading(false);
+        }
+
+        // return { submitted, loading, error, sendEmail }
     };
 
     const handleInputChange = (e) => {
         const { target } = e;
         const inputType = target.name;
         const inputValue = target.value;
+
+        setConfirmation(false);
+        target.classList.remove("invalid-entry");
 
         if (inputType === 'email') {
             setEmail(inputValue);
@@ -63,6 +66,7 @@ export default function Contact() {
             case "Email is invalid":
                 if (inputType === "email") {
                     setNotification("");
+                    
                 }
                 break;
             case "Please enter your name":
@@ -76,6 +80,9 @@ export default function Contact() {
                 }
                 break;
             default:
+                document.querySelectorAll(".invalid-entry").forEach((node) => {
+                    node.classList.remove("invalid-entry");
+                });
                 break;
         }
     };
@@ -83,6 +90,7 @@ export default function Contact() {
     const handleEmailLoseFocus = (e) => {
         if (!validateEmail(email)) {
             setNotification('Email is invalid');
+            e.target.classList.add("invalid-entry");
             return;
         }
     }
@@ -90,6 +98,7 @@ export default function Contact() {
     const handleNameLoseFocus = (e) => {
         if (!contactName) {
             setNotification("Please enter your name");
+            e.target.classList.add("invalid-entry");
             return;
         }
     }
@@ -99,26 +108,32 @@ export default function Contact() {
             setNotification(
                 "Please enter a message to send"
             );
+            e.target.classList.add("invalid-entry");
             return;
         }
     }
 
-    const handleFormSubmit = (e) => {
+    const handleFormSubmit = async (e) => {
         e.preventDefault();
 
         if (!validateEmail(email)) {
             setNotification('Email is invalid');
+            document.getElementById("email-input").classList.add("invalid-entry");
+            setConfirmation(false);
             return;
-
         }
         if (!contactName) {
             setNotification("Please enter your name");
+            document.getElementById("name-input").classList.add("invalid-entry");
+            setConfirmation(false);
             return;
         }
         if (!message) {
             setNotification(
                 "Please enter a message to send"
             );
+            document.getElementById("message-input").classList.add("invalid-entry");
+            setConfirmation(false);
             return;
         }
 
@@ -127,21 +142,31 @@ export default function Contact() {
             contactName,
             message
         }
+        // console.log("Line 145: ", loading, submitted);
+        setLoading(true);
 
-        sendEmail(data);
-        console.log(submitted, error, loading);
+        const response = await sendEmail(data);
+        // console.log("Line 149: ", loading, submitted, response);
 
+        if (response.status === 200) {
+            setSubmitted(true);
+            setLoading(false);
+        }
+        // console.log("Line 155: ", loading, submitted);
         setcontactName('');
         setMessage('');
         setEmail('');
         if (submitted) {
             setNotification("Thank you for reaching out! I will get back to you promptly.");
+            setConfirmation(true);
         }
         if (error) {
             setNotification(error);
+            setConfirmation(true);
         }
         if (loading) {
             setNotification("Sending your message...");
+            setConfirmation(true);
         }
 
     };
@@ -156,6 +181,7 @@ export default function Contact() {
                         <div className="control contact-input">
                             <input
                                 className="input"
+                                id="email-input"
                                 value={email}
                                 name="email"
                                 onChange={handleInputChange}
@@ -164,13 +190,14 @@ export default function Contact() {
                                 onBlur={handleEmailLoseFocus}
                             />
                         </div>
-                        {/* <p className="help is-danger">{notification}</p> */}
+                        {notification === "Email is invalid" && <p className="is-size-6 invalid">{notification}</p>}
                     </div>
                     <div className="field">
                         <label className="label">Name</label>
                         <div className="control contact-input">
                             <input
                                 className="input"
+                                id="name-input"
                                 value={contactName}
                                 name="contactName"
                                 onChange={handleInputChange}
@@ -179,12 +206,13 @@ export default function Contact() {
                                 onBlur={handleNameLoseFocus}
                             />
                         </div>
-                        {/* <p className="help is-danger">{notification}</p> */}
+                        {notification === "Please enter your name" && <p className="is-size-6 invalid">{notification}</p>}
                     </div>
                     <div className="field w-100">
                         <label className="label">Message</label>
                         <textarea
                             value={message}
+                            id="message-input"
                             name="message"
                             onChange={handleInputChange}
                             type="textarea"
@@ -192,13 +220,13 @@ export default function Contact() {
                             onBlur={handleMessageLoseFocus}
                             rows="5"
                         />
-                        {/* <p className="help is-danger">{notification}</p> */}
+                        {notification === "Please enter a message to send" && <p className="is-size-6 invalid">{notification}</p>}
                     </div>
                     <div className="control is-flex is-justify-content-flex-end my-5">
                         <input className="button is-small is-fullwidth is-size-5" id="contact-submit" type="submit" />
                     </div>
                 </form>
-                {notification && (
+                {notification && confirmation && (
                     <div className="error-div">
                         <p className="error-text">{notification}</p>
                     </div>
